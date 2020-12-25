1864147e9a2397c4282ce5ddfb9888c7893fc1cf
akshaiim <mishraakshaiii@gmail.com>
2020-12-25 23:50:59 +0530

    added saving to local storage functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./Form";
 import { Todos } from "./Todos";
 import "./index.css";
@@ -6,12 +6,36 @@ import "./index.css";
 const App = () => {
   const [data, setData] = useState("");
   const [todos, setTodos] = useState([]);
- 
+  useEffect(()=>{
+    getTodos();
+    
+  },[])
+  useEffect(()=>{
+    saveTodos();
+    
+  },[todos])
+
+
+  const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getTodos = () => {
+    if (localStorage.getItem("todos") == null) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+    else{
+      let todoLocal = JSON.parse(localStorage.getItem("todos"))
+      setTodos(todoLocal)
+    }
+  };
+  
+
   return (
     <>
       <h1>Todo App</h1>
       <Form todos={todos} setTodos={setTodos} setData={setData} data={data} />
-      <Todos  todos={todos} data={data} setTodos={setTodos} />
+      <Todos todos={todos} data={data} setTodos={setTodos} />
     </>
   );
 };
